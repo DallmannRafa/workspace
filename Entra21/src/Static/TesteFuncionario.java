effdb831741f5755150045fd4095b26a9e6148eb
Rafael <dallmann.rafa@gmail.com>
2019-07-25 20:24:56 -0300

    exercícios 58-62

--- Entra21/src/Static/TesteFuncionario.java
+++ Entra21/src/Static/TesteFuncionario.java
@@ -5,11 +5,12 @@ public class TesteFuncionario {
 	public static void main(String[] args) {
 		
 		Funcionario F1 = new Funcionario();
-		F1.ValeRefDiario = 12.56;
-		System.out.println(F1.ValeRefDiario);
+		
+		F1.setValeRefDiario(12.56);
+		System.out.println(F1.getValeRefDiario());
 		
 		F1.ReajusteVale(50);
-		System.out.println(F1.ValeRefDiario);
+		System.out.println(F1.getValeRefDiario());
 
 	}
 
